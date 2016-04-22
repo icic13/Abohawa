71577618970f274714b520d1ea1b5b5a95af1c06
Md. Rana Mahmud <ranamd0@gmail.com>
2016-04-22 11:59:53 +0600

    History function completed

--- src/abohawa/HistoryClass.java
+++ src/abohawa/HistoryClass.java
@@ -10,6 +10,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -17,6 +21,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableColumn;
 
 /**
  *
@@ -26,12 +31,11 @@ public class HistoryClass extends JFrame {
 
     public HistoryClass() {
         String[] columns = new String[]{
-            "Date", "Country", "Temperature", "Detail", "Pressure",
-            "Humidity"
+            "Date", "Country", "Temperature ℃", "Detail", "Pressure  hPa",
+            "Humidity %"
         };
         String[] result;
-        
-        
+        ArrayList<String> item = new ArrayList();
         try {
             FileReader read = new FileReader("data.txt");
             BufferedReader reader = new BufferedReader(read);
@@ -39,10 +43,8 @@ public class HistoryClass extends JFrame {
 
             try {
                 while ((line = reader.readLine()) != null) {
-                    result = line.split("#");
-                      for(int i = 0; i<result.length;i++)
-                          System.out.print(result[i]+" ");
-                      System.out.println();
+                    item.add(line);
+
                 }
             } catch (IOException ex) {
                 Logger.getLogger(Abohawa.class.getName()).log(Level.SEVERE, null, ex);
@@ -50,19 +52,48 @@ public class HistoryClass extends JFrame {
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Abohawa.class.getName()).log(Level.SEVERE, null, ex);
         }
-        Object[][] data = new Object[][]{
-            {1, "John", 40.0, false},
-            {2, "Rambo", 70.0, false},
-            {3, "Zorro", 60.0, true},};
+
+        String[][] data = new String[item.size()][6];
+
+        for (int i = 0; i < item.size(); i++) {
+            result = item.get(i).split("#");
+            String[] it = tokenizeData(result);
+
+            for (int j = 0; j < 6; j++) {
+
+                data[i][j] = it[j];
+
+            }
+
+        }
 
         //Table
-       // JTable table = new JTable(data, columns);
-        //this.add(new JScrollPane(table));
+        JTable table = new JTable(data, columns);
+        table.getColumnModel().getColumn(0).setPreferredWidth(50);
+        this.add(new JScrollPane(table));
         this.setTitle("Wether history");
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setLocationRelativeTo(null);
-        this.pack();
+
+        table.getColumnModel().getColumn(0).setPreferredWidth(150);
+
+        this.setSize(750, 300);
         this.setVisible(true);
+        
+
+    }
+
+    public String[] tokenizeData(String[] data) {
+
+        String cityFieldText = data[8].toUpperCase(Locale.US)
+                + ", " + data[19];
+
+        DateFormat df = DateFormat.getDateTimeInstance();
+        long value = Long.parseLong(data[0]);
+        String updatedOn = df.format(new Date(value * 1000));
+
+        String[] item = {updatedOn, cityFieldText, data[11], data[4].toUpperCase(Locale.US), data[13], data[14]};
+        return item;
 
     }
 
